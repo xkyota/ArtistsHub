f4f1a4b32b25779e964f2a691fa9085921088c52
Maksym Durikhin <max1mkasss.mail@gmail.com>
2025-07-02 23:42:45 +0300

    +

--- src/js/feedback-section.js
+++ src/js/feedback-section.js
@@ -1,52 +1,47 @@
+const feedback = document.querySelector('.feedback');
 
-const feedback = document.querySelector('.feedback')
-fetch("https://sound-wave.b.goit.study/api/feedbacks")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    data.data.forEach(element => {
-      console.log(element)
+fetch('https://sound-wave.b.goit.study/api/feedbacks')
+  .then(response => response.json())
+    .then(data => {
+        data.data.forEach(element => {
+              feedback.insertAdjacentHTML(
+                      'beforeend',
+                              `
+                                    <div class="feedback-item">
+                                            <div class="stars">
+                                                      <span class="star" data-value="1"></span>
+                                                                <span class="star" data-value="2"></span>
+                                                                          <span class="sta} " data-value="3"></span>
+                                                                                    <span class="star" data-value="4"></span>
+                                                                                              <span class="star" data-value="5"></span>
+                                                                                                      </div>
+                                                                                                              <p class="item-coment">${element.descr}</p>
+                                                                                                                      <p class="item-person">${element.name}</p>
+                                                                                                                            </div>
+                                                                                                                                  `
+                                                                                                                                        );
 
-      feedback.insertAdjacentHTML('beforeend', `
-      <div class="feedback-item">
-        <div class="stars">
-  <span class="star" data-value="1"></span>
-  <span class="star" data-value="2"></span>
-  <span class="star" data-value="3"></span>
-  <span class="star" data-value="4"></span>
-  <span class="star" data-value="5"></span>
-</div>
-        <p class="item-coment">${element.descr}</p>
-        <p class="item-person">${element.name}</p>
-    </div>
-    `)
-    const lastItem = feedback.lastElementChild;
-    fillStars(lastItem, element.rating);
-    });
-  })
-  .catch(error => {
-    console.log(error);
-  });
+                                                                                                                                              const lastItem = feedback.lastElementChild;
+                                                                                                                                                    fillStars(lastItem, element.rating);
+                                                                                                                                                        });
+                                                                                                                                                          })
+                                                                                                                                                            .catch(error => {
+                                                                                                                                                                console.log(error);
+                                                                                                                                                                  });
 
+                                                                                                                                                                  function fillStars(container, rating) {
+                                                                                                                                                                    const stars = container.querySelectorAll('.star');
 
+                                                                                                                                                                      stars.forEach((star, index) => {
+                                                                                                                                                                          let fillPercent = 0;
 
-function fillStars(rating) {
-  const stars = document.querySelectorAll('.star');
+                                                                                                                                                                              if (index + 1 <= rating) {
+                                                                                                                                                                                    fillPercent = 100;
+                                                                                                                                                                                        } else if (index < rating) {
+                                                                                                                                                                                              const decimal = rating - index;
+                                                                                                                                                                                                    fillPercent = Math.round(decimal * 100);
+                                                                                                                                                                                                        }
 
-  stars.forEach((star, index) => {
-    let fillPercent = 0;
-
-    if (index + 1 <= rating) {
-      fillPercent = 100;
-    } else if (index < rating) {
-      const decimal = rating - index;
-      fillPercent = Math.round(decimal * 100);
-    }
-    star.style.setProperty('--fill', `${fillPercent}%`);
-  });
-}
-
-fillStars(`${element.rating}`) 
-
- 
+                                                                                                                                                                                                            star.style.setProperty('--fill', `${fillPercent}%`);
+                                                                                                                                                                                                              });
+                                                                                                                                                                                                              }
