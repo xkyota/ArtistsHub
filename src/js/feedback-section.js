c3c44f5c0fd64647da5b1142265685401c99a994
PvNastia <nastiapogorelova6@gmail.com>
2025-07-03 21:26:10 +0300

    feedback-section swiper

--- src/js/feedback-section.js
+++ src/js/feedback-section.js
@@ -6,14 +6,13 @@ fetch('https://sound-wave.b.goit.study/api/feedbacks')
     data.data.forEach(element => {
       feedback.insertAdjacentHTML(
         'beforeend',
-        `  <div class="feedback-item">
+        `  <div class="feedback-item swiper-slide">
               <div class="stars">
                 <span class="star" data-value="1"></span>
                 <span class="star" data-value="2"></span>
-                <span class="sta} " data-value="3"></span>
+                <span class="star " data-value="3"></span>
                 <span class="star" data-value="4"></span>
                 <span class="star" data-value="5"></span>
-                    <span class="star" data-value="5"></span>
               </div>
               <p class="item-coment">${element.descr}</p>
               <p class="item-person">${element.name}</p>
@@ -23,6 +22,26 @@ fetch('https://sound-wave.b.goit.study/api/feedbacks')
       const lastItem = feedback.lastElementChild;
       fillStars(lastItem, element.rating);
     });
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+    
   })
   .catch(error => {
     console.log(error);
@@ -44,16 +63,3 @@ function fillStars(container, rating) {
     star.style.setProperty('--fill', `${fillPercent}%`);
   });
 }
-
-const arrowLeft = document.querySelector('.arrowLeft');
-const arrowRight = document.querySelector('.arrowRight');
-
-const scrollAmount = 1500;
-
-arrowLeft.addEventListener('click', () => {
-  feedback.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-});
-
-arrowRight.addEventListener('click', () => {
-  feedback.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-});
