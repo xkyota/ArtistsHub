e564aa153316e12d97a4b2c3d03e1264ca548e55
danikkk <danilmalyk20@gmail.com>
2025-07-24 19:14:08 +0300

    +

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/header.js';
+import './js/hero-section.js';
+import './js/artists-section.js';
+import './js/artist-details-domal.js';
+import './js/about-us-section.js';
+import './js/feedback-section.js';
+import './js/footer.js';
--- src/partials-loader.js
+++ src/partials-loader.js
@@ -1,33 +1,33 @@
-(async () => {
-  const partials = document.querySelectorAll("load[src]");
+// (async () => {
+//   const partials = document.querySelectorAll("load[src]");
 
-  for (const el of partials) {
-    const url = el.getAttribute("src");
-    const res = await fetch(url);
-    const html = await res.text();
-    const container = document.createElement("div");
-    container.innerHTML = html;
-    el.replaceWith(...container.childNodes);
-  }
+//   for (const el of partials) {
+//     const url = el.getAttribute("src");
+//     const res = await fetch(url);
+//     const html = await res.text();
+//     const container = document.createElement("div");
+//     container.innerHTML = html;
+//     el.replaceWith(...container.childNodes);
+//   }
 
-  const scripts = [
-    "./js/header.js",
-    "./js/hero-section.js",
-    "./js/artists-section.js",
-    "./js/artist-details-domal.js",
-    "./js/about-us-section.js",
-    "./js/feedback-section.js",
-    "./js/footer.js",
-  ];
+//   const scripts = [
+//     "./js/header.js",
+//     "./js/hero-section.js",
+//     "./js/artists-section.js",
+//     "./js/artist-details-domal.js",
+//     "./js/about-us-section.js",
+//     "./js/feedback-section.js",
+//     "./js/footer.js",
+//   ];
 
-  for (const src of scripts) {
-    await new Promise((resolve, reject) => {
-      const script = document.createElement("script");
-      script.src = src;
-      script.type = "module";
-      script.onload = resolve;
-      script.onerror = reject;
-      document.body.appendChild(script);
-    });
-  }
-})();
+//   for (const src of scripts) {
+//     await new Promise((resolve, reject) => {
+//       const script = document.createElement("script");
+//       script.src = src;
+//       script.type = "module";
+//       script.onload = resolve;
+//       script.onerror = reject;
+//       document.body.appendChild(script);
+//     });
+//   }
+// })();
